5cc882aa63137c346d6803ba3ae67dcb9483e97a
oooleks <oooleks@gmail.com>
2020-12-03 20:53:47 -0600

    Add BaseTest

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,24 +1,21 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import runner.BaseTest;
 
-public class SimpleTest {
+public class SimpleTest extends BaseTest {
 
     @Test
     public void simpleTest() throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "C:/Driver/chromedriver.exe");
-
-        WebDriver browser = new ChromeDriver();
-        browser.get("https://github.com/oooleks/PlatformaticaQA");
+        WebDriver browser = getDriver();
+        browser.get("https://github.com/SergeiDemyanenko/PlatformaticaQA");
         WebElement name = browser.findElement(By.xpath("//strong/a"));
 
         Assert.assertEquals(name.getText(), "PlatformaticaQA");
 
-        Thread.sleep(1000);
-        browser.quit();
+        Thread.sleep(3000);
     }
 }
