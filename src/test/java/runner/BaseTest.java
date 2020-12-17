ee08eab935318fd08f49d78c405574dcebda0f4f
oooleks <oooleks@gmail.com>
2020-12-17 15:03:23 -0600

    Fix Assert

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -3,22 +3,26 @@ package runner;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public abstract class BaseTest {
 
-    // Selenium Grid
     public static final String HUB_URL = "http://localhost:4444/wd/hub";
 
-    // check if remote driver is running on 4444 port
+    private static final Logger logger = Logger.getLogger("BaseLogger");
     private static boolean remoteWebDriver = false;
     static {
         try {
@@ -26,16 +30,14 @@ public abstract class BaseTest {
             try {
                 con.setRequestMethod("GET");
                 remoteWebDriver = con.getResponseCode() == HttpURLConnection.HTTP_OK;
-                // finally section will always execute -> disconnect connection
             } finally {
                 con.disconnect();
             }
-        } catch (IOException ignore) {
-            // code if error occurs
-        }
+        } catch (IOException ignore) {}
 
         if (!remoteWebDriver) {
             WebDriverManager.chromedriver().setup();
+
         }
     }
 
@@ -46,7 +48,7 @@ public abstract class BaseTest {
 
         if (remoteWebDriver) {
             try {
-                this.driver = new RemoteWebDriver(new URL(HUB_URL), DesiredCapabilities.chrome());
+                this.driver = new RemoteWebDriver(new URL(HUB_URL), new ChromeOptions());
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             }
@@ -54,15 +56,23 @@ public abstract class BaseTest {
             this.driver = new ChromeDriver();
         }
 
+        this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         this.driver.manage().window().maximize();
     }
 
-//    @AfterMethod
-    protected void setDownAll() {
+    @AfterMethod
+    protected void setDownAll(Method method, ITestResult tr) {
         driver.quit();
+        long executionTime = (tr.getEndMillis() - tr.getStartMillis()) / 1000;
+        this.log(String.format("\u001B[33m%s.%s() Execution time: %ds\u001B[0m",
+                this.getClass(), method.getName(), executionTime));
     }
 
     protected WebDriver getDriver() {
         return driver;
+    }
+
+    protected void log(String message) {
+        logger.log(Level.INFO, message);
     }
 }
